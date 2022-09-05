9f4336cde4ea9ca0dd093973ba2aad9146ed3091
jazmin saleh peña <jazminasaleh@gmail.com>
2022-09-05 15:18:34 -0500

    pagina 1

--- lib/src/widgets/lista_noticias.dart
+++ lib/src/widgets/lista_noticias.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:noticias/src/models/news_models.dart';
+import 'package:noticias/src/theme/tema.dart';
+
 //la lista de las noticias
 class ListaNoticias extends StatelessWidget {
   final List<Article> noticias;
@@ -11,8 +13,141 @@ class ListaNoticias extends StatelessWidget {
     return ListView.builder(
       itemCount: this.noticias.length,
       itemBuilder: (BuildContext context, int index) {
-        return Text(this.noticias[index].title!);
+        return _Noticia(this.noticias[index], index);
       },
+    );
+  }
+}
+
+class _Noticia extends StatelessWidget {
+  final Article noticia;
+  final int index;
+
+  const _Noticia(this.noticia, this.index);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _TarjetaTopBar(
+          noticia: this.noticia,
+          index: this.index,
+        ),
+        _TarjetaTitulo(noticia: noticia),
+        _TarjetaImagen(noticia: noticia),
+        _TarjetaBody(noticia: noticia),
+        _TarjetaBotones(),
+        SizedBox(height: 10,),
+        Divider(),
+      ],
+    );
+  }
+}
+
+class  _TarjetaBotones extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          RawMaterialButton(
+            onPressed: (){},
+            fillColor: miTema.accentColor,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20),
+            ),
+            child: Icon(Icons.star_border),
+          ),
+          SizedBox(width: 10,),
+           RawMaterialButton(
+            onPressed: (){},
+            fillColor: Colors.blue,
+            shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20),
+            ),
+            child: Icon(Icons.more),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class  _TarjetaBody extends StatelessWidget {
+  final Article noticia;
+  const  _TarjetaBody({required this.noticia});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text(noticia.description!),
+    );
+  }
+}
+
+class _TarjetaImagen extends StatelessWidget {
+  final Article noticia;
+  const _TarjetaImagen({required this.noticia});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10),
+      child: ClipRRect(
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(50), bottomRight: Radius.circular(50)),
+        child: Container(
+          child: (noticia.urlToImage != null)
+            ?FadeInImage(
+              image: NetworkImage(noticia.urlToImage!),
+              placeholder:AssetImage('assets/giphy.gif') ,)
+            :Image(image: AssetImage('assets/no-img.png')) 
+        ),
+      ),
+    );
+  }
+}
+
+class _TarjetaTitulo extends StatelessWidget {
+  final Article noticia;
+
+  const _TarjetaTitulo({required this.noticia});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 15),
+      child: Text(
+        noticia.title!,
+        style: TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
+      ),
+    );
+  }
+}
+
+class _TarjetaTopBar extends StatelessWidget {
+  final Article noticia;
+  final int index;
+
+  const _TarjetaTopBar({
+    required this.noticia,
+    required this.index,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 10),
+      margin: EdgeInsets.only(bottom: 10),
+      child: Row(
+        children: [
+          Text(
+            '${index + 1}.',
+            style: TextStyle(color: miTema.accentColor),
+          ),
+          Text(
+            '${noticia.source.name}.',
+          )
+        ],
+      ),
     );
   }
 }
